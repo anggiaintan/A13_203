48acf38125739ef4d1f60a4e6c780e6da6a04505
Anggia Intan <intananggia23@gmail.com>
2025-01-20 13:48:39 +0700

    membuat data class PesertaDetailResponse

--- app/src/main/java/com/example/projectpam/model/Peserta.kt
+++ app/src/main/java/com/example/projectpam/model/Peserta.kt
@@ -1,6 +1,14 @@
 package com.example.projectpam.model
 import kotlinx.serialization.Serializable
 
+
+@Serializable
+data class PesertaDetailResponse (
+    val status: Boolean,
+    val message: String,
+    val data: Peserta
+)
+
 @Serializable
 data class Peserta (
     val id_peserta: Int,
