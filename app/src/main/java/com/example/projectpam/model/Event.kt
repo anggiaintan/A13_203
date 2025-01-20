ed8d7bbaf290cf1355fc530ad32db2ad56b9b841
Anggia Intan <intananggia23@gmail.com>
2025-01-20 14:07:06 +0700

    membuat data class EventDetailResponse

--- app/src/main/java/com/example/projectpam/model/Event.kt
+++ app/src/main/java/com/example/projectpam/model/Event.kt
@@ -3,6 +3,13 @@ package com.example.projectpam.model
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class EventDetailResponse (
+    val status: Boolean,
+    val message: String,
+    val data: Event
+)
+
+@Serializable
 data class Event (
     val id_event: Int,
     val nama_event: String,
