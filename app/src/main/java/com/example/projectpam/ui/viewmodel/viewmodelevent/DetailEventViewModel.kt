25c42ba6013805c761d34b346cc93174a50b5ac7
Anggia Intan <intananggia23@gmail.com>
2025-01-22 15:05:53 +0700

    membuat DetailEventViewModel

---
+++ app/src/main/java/com/example/projectpam/ui/viewmodel/viewmodelevent/DetailEventViewModel.kt
@@ -0,0 +1,50 @@
+package com.example.projectpam.ui.viewmodel.viewmodelevent
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.projectpam.model.Event
+import com.example.projectpam.repository.EventRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+sealed class DetailUiState {
+    data class Success (val event: Event) : DetailUiState()
+    object Error : DetailUiState()
+    object Loading : DetailUiState()
+}
+
+class DetailEventViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val evn: EventRepository
+): ViewModel() {
+    private val _id_Event: Int = checkNotNull(savedStateHandle[DestinasiDetail.ID_EVENT])
+    private val _detailUiState = MutableStateFlow<DetailUiState>(DetailUiState.Loading)
+    val detailUiState: StateFlow<DetailUiState> = _detailUiState
+    init { getDetailEvent() }
+    fun getDetailEvent() {
+        viewModelScope.launch {
+            try {
+                _detailUiState.value = DetailUiState.Loading
+                val event = evn.getEventById(_id_Event)
+                if (event != null) {
+                    _detailUiState.value = DetailUiState.Success(event)
+                } else {
+                    _detailUiState.value = DetailUiState.Error }
+            } catch (e: Exception) {
+                _detailUiState.value = DetailUiState.Error
+            }
+        }
+    }
+}
+
+fun Event.toDetailUiEvent(): InsertUiEvent {
+    return InsertUiEvent (
+        id_event = id_event,
+        nama_event = nama_event,
+        deskripsi_event = deskripsi_event,
+        tanggal_event = tanggal_event,
+        lokasi_event = lokasi_event
+    )
+}
