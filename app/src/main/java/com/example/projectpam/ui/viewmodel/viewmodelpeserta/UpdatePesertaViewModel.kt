7599e0d7976125acb972b7300af51f77ad68f3b5
Anggia Intan <intananggia23@gmail.com>
2025-01-22 10:55:01 +0700

    membuat class UpdatePesertaViewModel

---
+++ app/src/main/java/com/example/projectpam/ui/viewmodel/viewmodelpeserta/UpdatePesertaViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.projectpam.ui.viewmodel.viewmodelpeserta
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.projectpam.model.Peserta
+import com.example.projectpam.repository.PesertaRepository
+import kotlinx.coroutines.launch
+
+class UpdatePesertaViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val psrta: PesertaRepository
+): ViewModel()
+{
+    val id_peserta: Int = checkNotNull(savedStateHandle[DestinasiUpdate.ID_PESERTA])
+    var uiState = mutableStateOf(InsertUiState())
+    init {getPeserta()}
+    private fun getPeserta(){
+        viewModelScope.launch {
+            try {
+                val peserta = psrta.getPesertaById(id_peserta)
+                peserta?.let {
+                    uiState.value = it.toInsertUIEvent()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+    fun updatePeserta(id_peserta: Int, peserta: Peserta) {
+        viewModelScope.launch {
+            try {
+                psrta.updatePeserta(id_peserta, peserta)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun updatePesertaState(insertUiEvent: InsertUiEvent) {
+        uiState.value = uiState.value.copy(insertUiEvent = insertUiEvent)
+    }
+}
+fun Peserta.toInsertUIEvent(): InsertUiState = InsertUiState (
+    insertUiEvent = this.toDetailUiEvent()
+)
