c8eae0f5e3e6c2fe176fa290cc58e09d96f1ee90
Anggia Intan <intananggia23@gmail.com>
2025-01-22 15:18:16 +0700

    membuat UpdateEventViewModel

---
+++ app/src/main/java/com/example/projectpam/ui/viewmodel/viewmodelevent/UpdateEventViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.projectpam.ui.viewmodel.viewmodelevent
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.projectpam.model.Event
+import com.example.projectpam.repository.EventRepository
+import kotlinx.coroutines.launch
+
+class UpdateEventViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val evn: EventRepository
+): ViewModel()
+{
+    val id_event: Int = checkNotNull(savedStateHandle[DestinasiUpdate.ID_EVENT])
+    var uiState = mutableStateOf(InsertUiState())
+    init {getEvent()}
+    private fun getEvent(){
+        viewModelScope.launch {
+            try {
+                val event = evn.getEventById(id_event)
+                event?.let {
+                    uiState.value = it.toInsertUIEvent()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+    fun updateEvent(id_event: Int, event: Event) {
+        viewModelScope.launch {
+            try {
+                evn.updateEvent(id_event, event)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun updateEventState(insertUiEvent: InsertUiEvent) {
+        uiState.value = uiState.value.copy(insertUiEvent = insertUiEvent)
+    }
+}
+fun Event.toInsertUIEvent(): InsertUiState = InsertUiState (
+    insertUiEvent = this.toDetailUiEvent()
+)
