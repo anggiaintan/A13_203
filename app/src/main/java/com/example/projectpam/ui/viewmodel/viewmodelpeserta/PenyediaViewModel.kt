b239442e11572feb92c50200aa81dffedb9b0642
Anggia Intan <intananggia23@gmail.com>
2025-01-23 14:35:16 +0700

    menambahkan initializer dari beberapa fungsi yang sudah dibuat

--- app/src/main/java/com/example/projectpam/ui/viewmodel/viewmodelpeserta/PenyediaViewModel.kt
+++ app/src/main/java/com/example/projectpam/ui/viewmodel/viewmodelpeserta/PenyediaViewModel.kt
@@ -7,6 +7,17 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.projectpam.TiketkuApplication
+import com.example.projectpam.ui.viewmodel.viewmodelevent.DetailEventViewModel
+import com.example.projectpam.ui.viewmodel.viewmodelevent.HomeEventViewModel
+import com.example.projectpam.ui.viewmodel.viewmodelevent.InsertEventViewModel
+import com.example.projectpam.ui.viewmodel.viewmodelevent.UpdateEventViewModel
+import com.example.projectpam.ui.viewmodel.viewmodeltiket.DetailTiketViewModel
+import com.example.projectpam.ui.viewmodel.viewmodeltiket.HomeTiketViewModel
+import com.example.projectpam.ui.viewmodel.viewmodeltiket.InsertTiketViewModel
+import com.example.projectpam.ui.viewmodel.viewmodeltiket.UpdateTiketViewModel
+import com.example.projectpam.ui.viewmodel.viewmodeltransaksi.DetailTransaksiViewModel
+import com.example.projectpam.ui.viewmodel.viewmodeltransaksi.HomeTransaksiViewModel
+import com.example.projectpam.ui.viewmodel.viewmodeltransaksi.InsertTransaksiViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
@@ -14,6 +25,17 @@ object PenyediaViewModel {
         initializer { InsertPesertaViewModel (aplikasiTiketku().container.pesertaRepository) }
         initializer { DetailPesertaViewModel (createSavedStateHandle(), aplikasiTiketku().container.pesertaRepository) }
         initializer { UpdatePesertaViewModel (createSavedStateHandle(), aplikasiTiketku().container.pesertaRepository) }
+        initializer { HomeEventViewModel (aplikasiTiketku().container.eventRepository) }
+        initializer { InsertEventViewModel (aplikasiTiketku().container.eventRepository) }
+        initializer { DetailEventViewModel (createSavedStateHandle(), aplikasiTiketku().container.eventRepository) }
+        initializer { UpdateEventViewModel (createSavedStateHandle(), aplikasiTiketku().container.eventRepository) }
+        initializer { HomeTiketViewModel (aplikasiTiketku().container.tiketRepository) }
+        initializer { InsertTiketViewModel (aplikasiTiketku().container.tiketRepository) }
+        initializer { DetailTiketViewModel (createSavedStateHandle(), aplikasiTiketku().container.tiketRepository) }
+        initializer { UpdateTiketViewModel (createSavedStateHandle(), aplikasiTiketku().container.tiketRepository) }
+        initializer { HomeTransaksiViewModel (aplikasiTiketku().container.transaksiRepository) }
+        initializer { InsertTransaksiViewModel (aplikasiTiketku().container.transaksiRepository) }
+        initializer { DetailTransaksiViewModel (createSavedStateHandle(), aplikasiTiketku().container.transaksiRepository) }
     }
 }
 
