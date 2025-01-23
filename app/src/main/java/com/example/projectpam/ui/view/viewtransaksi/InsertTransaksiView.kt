c16287193771e138cac80ace2e9fc8ece7989a5f
Anggia Intan <intananggia23@gmail.com>
2025-01-23 14:23:07 +0700

    membuat InsertTransaksiView

---
+++ app/src/main/java/com/example/projectpam/ui/view/viewtransaksi/InsertTransaksiView.kt
@@ -0,0 +1,156 @@
+package com.example.projectpam.ui.view.viewtransaksi
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.projectpam.navigation.DestinasiNavigasi
+import com.example.projectpam.ui.customwidget.CostumeTopAppBar
+import com.example.projectpam.ui.viewmodel.viewmodelpeserta.PenyediaViewModel
+import com.example.projectpam.ui.viewmodel.viewmodeltransaksi.InsertTransaksiViewModel
+import com.example.projectpam.ui.viewmodel.viewmodeltransaksi.InsertUiEvent
+import com.example.projectpam.ui.viewmodel.viewmodeltransaksi.InsertUiState
+import kotlinx.coroutines.launch
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Insert Transaksi"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryTransaksiScreen (
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertTransaksiViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) {
+            innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onTransaksiValueChange = viewModel::updateInsertTransaksiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertTransaksi()
+                    navigateBack()
+                }
+            }, modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun EntryBody (
+    insertUiState: InsertUiState,
+    onTransaksiValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column (
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onTransaksiValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField (
+            value = insertUiEvent.id_transaksi,
+            onValueChange = {onValueChange(
+                insertUiEvent.copy(id_transaksi = it)
+            )},
+            label = { Text("ID Transaksi") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField (
+            value = insertUiEvent.id_tiket,
+            onValueChange = {onValueChange(insertUiEvent.copy(id_tiket = it))},
+            label = { Text("ID Tiket") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField (
+            value = insertUiEvent.jumlah_tiket.toString(),
+            onValueChange = {onValueChange(insertUiEvent.copy(jumlah_tiket = it.toIntOrNull() ?: 0))},
+            label = { Text("Jumlah tiket") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField (
+            value = insertUiEvent.jumlah_pembayaran,
+            onValueChange = {onValueChange(insertUiEvent.copy(jumlah_pembayaran = it))},
+            label = { Text("Jumlah pembayaran") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled) {
+            Text(
+                text = "Isi semua data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
